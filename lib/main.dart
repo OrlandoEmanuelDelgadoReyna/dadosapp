af40044ab5324886c1c96b41de4896113443dc82
Orlando Emanuel <7271983841@untrm.edu.pe>
2023-10-27 21:10:55 -0500

    prueba2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TheApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class TheApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
